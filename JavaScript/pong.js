e950ea0903f9db04fd2886828453246221cbd43f
Lmazenc34070 <lucas.mazenc31@gmail.com>
2020-11-03 14:57:30 +0100

    Comm

--- JavaScript/pong.js
+++ JavaScript/pong.js
@@ -1,11 +1,11 @@
 let largeur=$("#balle").width();
 let gauche=parseInt($("#balle").css("left"));
 let haut=parseInt($("#balle").css("top"));
-alert(gauche);
+alert(gauche); //Message d'alerte à l'ouverture de la page indiquant la position de la balle
 
 setInterval(function(){
-    gauche=gauche+1;
-    haut=haut+0.5;
+    gauche=gauche+1; //Donne un mouvement à la balle vers la droite
+    haut=haut+0.5; //Donne un mouvement à la balle vers le bas
     $("#balle").css("left",gauche);
     $("#balle").css("top",haut);
 }, 10);
