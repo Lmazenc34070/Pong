5d36036f892066ec69b5575cb200ebf3b419c63c
Lmazenc34070 <lucas.mazenc31@gmail.com>
2020-11-28 02:22:52 +0100

    Changement de balle.js

--- JavaScript/Balle.js
+++ JavaScript/Balle.js
@@ -1,41 +1,75 @@
+//Classe de création d'un objet balle
 class Balle {
     /**
+     *
      * @param $html
      */
     constructor($html) {
         this.$element = $html;
+
         /**
+         *
          * @type {number}
          */
         this.hauteur = $html.height();
+
         /**
+         *
          * @type {number}
          */
         this.largeur = $html.width();
+
         /**
+         *
          * @type {number}
          */
         this.gauche = parseInt($html.css("left"));
+
         /**
+         *
          * @type {number}
          */
         this.haut = parseInt($html.css("top"));
+
         /**
+         *
          * @type {number}
          */
-        this.vitesseX = 2;
+        this.vitesseBase = terrain.largeur / 500;
+
         /**
+         *
          * @type {number}
          */
-        this.vitesseY = 0.5;
+        this.vitesse = this.vitesseBase;
+
         /**
+         *
          * @type {number}
          */
-        this.angle = Math.random() * 2 * Math.PI;
+        this.vitesseMax = terrain.largeur / 100;
+
+        /**
+         *
+         * @type {number}
+         */
+        this.acceleration = 1.2;
+
+        /**
+         *
+         * @type {number}
+         */
+        this.buf = Math.random();
+
+        /**
+         *
+         * @type {number}
+         */
+        this.angle = this.defAngle();
     }
 
     /**
-     * Le get permet d'otenir une valeur
+     *
      * @returns {number}
      */
     get bas() {
@@ -43,7 +77,7 @@ class Balle {
     }
 
     /**
-     * Le set permet d'initialiser une valeur
+     *
      * @param value
      */
     set bas(value) {
@@ -51,7 +85,7 @@ class Balle {
     }
 
     /**
-     * Le get permet d'otenir une valeur
+     *
      * @returns {number}
      */
     get droite() {
@@ -59,80 +93,134 @@ class Balle {
     }
 
     /**
-     * Le set permet d'initialiser une valeur
+     *
      * @param value
      */
     set droite(value) {
         this.gauche = value - this.largeur;
     }
 
-    bouger() {
-        this.gauche += Math.cos(this.angle) * this.vitesseX;
-        this.haut += Math.sin(this.angle) * this.vitesseY;
+    /**
+     *
+     * @returns {number}
+     */
+    defAngle()
+    {
+        return this.buf < 0.5 ? (5*Math.PI/4) - Math.random()*(2*Math.PI/4) : (Math.PI/4) - Math.random()*(2*Math.PI/4);
+    }
 
-        this.rebonds();
+    /**
+     *Augmente la vitesse de la balle jusqu'à un maximum
+     */
+    accelerer() {
+        if (Math.abs(this.vitesse) < this.vitesseMax)
+        {
+            this.vitesse *= this.acceleration;
+            console.log(Math.abs(this.vitesse));
+        }
+        else{
+            this.vitesse = this.vitesseMax;
+        }
+    }
+
+    /**
+     *Recentre la balle au milieu du terrain
+     */
+    recentrer() {
+        this.gauche = terrain.largeur / 2 - this.largeur / 2;
+        this.haut = terrain.hauteur / 2 - this.hauteur / 2;
+
+        this.angle = this.defAngle();
+        this.vitesseDeBase();
+    }
+
+    /**
+     *Remet la vitesse de la balle à celle de départ
+     */
+    vitesseDeBase() {
+        this.vitesse = this.vitesseBase;
+    }
+
+    /**
+     *Fait se déplacer la balle
+     */
+    bouger() {
+        this.gauche += Math.cos(this.angle) * this.vitesse;
+        this.haut += Math.sin(this.angle) * this.vitesse;
+        console.log(this.vitesse);
+
+        this.limite();
         this.majHTML();
     }
 
-    rebonds() {
+    /**
+     *Défini les limites que la balle ne doit pas dépasser
+     */
+    limite() {
         //droite
         if ((this.droite) > terrain.largeur) {
             terrain.tiltDroite();
+
+            this.buf = 0.75;
+
             this.droite = terrain.largeur;
-            this.vitesseX *= -1;
+            this.angle = Math.PI - this.angle;
             this.recentrer();
         }
         //gauche
         if (this.gauche < 0) {
             terrain.tiltGauche();
+
+            this.buf = 0.25;
+
             this.gauche = 0;
-            this.vitesseX *= -1;
+            this.angle = Math.PI - this.angle;
             this.recentrer();
         }
         //bas
         if (this.bas > terrain.hauteur) {
             terrain.tiltBas();
+
             this.bas = terrain.hauteur;
-            this.vitesseY *= -1;
+            this.angle = -(this.angle);
         }
         //haut
         if (this.haut < 0) {
             terrain.tiltHaut();
+
             this.haut = 0;
-            this.vitesseY *= -1;
+            this.angle = -(this.angle);
         }
         //Rebonds sur les raquettes
         //Gauche
-        if(this.gauche < raquetteGauche.droite){ //si la balle dépasse à gauche de la raquette gauche
-            if(this.bas > raquetteGauche.haut){ //et si la balle est plus basse que le haut de la raquette
-                if(this.haut < raquetteGauche.bas){ // et si la balle est plus haute que le bas de la raquette
+        if (this.gauche < raquetteGauche.droite) { //si la balle dépasse à gauche de la raquette gauche
+            if (this.bas > raquetteGauche.haut) { //et si la balle est plus basse que le haut de la raquette
+                if (this.haut < raquetteGauche.bas) { // et si la balle est plus haute que le bas de la raquette
+
+                    this.accelerer();
+                    this.angle = Math.PI - this.angle;
+
                     raquetteGauche.tiltRGauche();
-                    this.vitesseX *= -1;
-                    if(this.vitesseY <= 8){
-                        this.vitesseY += 1;
-                    }
                 }
             }
         }
         //Droite
-        if(this.droite > raquetteDroite.gauche){ //si la balle dépasse à droite la raquette droite
-            if(this.bas > raquetteDroite.haut){ //et si la balle est plus basse que le haut de la raquette
-                if(this.haut < raquetteDroite.bas){ // et si la balle est plus haute que le bas de la raquette
+        if (this.droite > raquetteDroite.gauche) { //si la balle dépasse à droite la raquette droite
+            if (this.bas > raquetteDroite.haut) { //et si la balle est plus basse que le haut de la raquette
+                if (this.haut < raquetteDroite.bas) { // et si la balle est plus haute que le bas de la raquette
+
+                    this.accelerer();
+                    this.angle = Math.PI - this.angle;
+
                     raquetteDroite.tiltRDroite();
-                    this.vitesseX *= -1;
-                    if(this.vitesseY <= 8){
-                        this.vitesseY += 1;
-                    }
                 }
             }
         }
     }
 
-    recentrer() {
-        this.gauche = terrain.largeur / 2 - this.largeur / 2;
-        this.haut = terrain.hauteur / 2 - this.hauteur / 2;
-    }
-
+    /**
+     *
+     */
     majHTML() {
         this.$element.css("left", balle.gauche);
         this.$element.css("top", balle.haut);
